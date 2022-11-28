4181b2c021f492023e5f91832a0957dd9266438d
Clint Grove <clintelyonson@gmail.com>
2022-11-28 13:31:30 +0000

    sql dbs folder

---
+++ SQL DBs/DatabaseLIDA/tblAuthors.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblAuthors]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Author_name] NCHAR(155) NULL, 
+    [country] NCHAR(101) NULL
+)
---
+++ SQL DBs/dafacstuffuks-target/customer_table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[customer_table](
+	[PersonID] [int] NOT NULL PRIMARY KEY,
+	[Name] [varchar](250) NULL,
+	[LastModifytime] [datetime] NULL
+) 
