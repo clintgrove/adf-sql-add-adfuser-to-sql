a064fb14fb855916ca0efe60fe79cf7550feef97
Clint Grove <clintelyonson@gmail.com>
2023-01-31 12:01:57 +0000

    full ps1 back

--- DevOps/Tasks/Utils/AddSGObjectidToSQLserverAsUser.ps1
+++ DevOps/Tasks/Utils/AddSGObjectidToSQLserverAsUser.ps1
@@ -14,34 +14,34 @@ $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmP
 $sqlToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://database.windows.net").AccessToken
 Write-Host ("##vso[task.setvariable variable=SQLTOKEN;]$sqlToken")  
 
-# Write-Verbose "Get SID"
-# $objId=$aadGroupObjectId.Trim('"')
-# function ConvertTo-Sid {
-# param (
-# [string]$objectId
-# )
-# [guid]$guid = [System.Guid]::Parse($objectId)
-# foreach ($byte in $guid.ToByteArray()) {
-# $byteGuid += [System.String]::Format("{0:X2}", $byte)
-# }
-# return "0x" + $byteGuid
-# }
-# $SID=ConvertTo-Sid($objId)
-# Write-Verbose "Create SQL connectionstring"
-# $conn = New-Object System.Data.SqlClient.SQLConnection
-# $conn.ConnectionString = "Server=$db_server;Initial Catalog=$db_name;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30"
-# $conn.AccessToken = $sqlToken
-# Write-Verbose "Connect to database and execute SQL script"
-# $conn.Open()
-# $query = "IF NOT EXISTS (SELECT [name]
-# FROM [sys].[database_principals]
-# WHERE [type] = N'X' AND [name] = N'$aadGroupName')
-# BEGIN
-# CREATE USER [$aadGroupName] WITH DEFAULT_SCHEMA=[dbo], SID = $SID, TYPE= X;
-# ALTER ROLE [$dbRoleName] ADD MEMBER [$aadGroupName];
-# END
-# "
-# Write-Host $query
-# $command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $conn)
-# $Result = $command.ExecuteNonQuery()
-# $conn.Close()
+Write-Verbose "Get SID"
+$objId=$aadGroupObjectId.Trim('"')
+function ConvertTo-Sid {
+param (
+[string]$objectId
+)
+[guid]$guid = [System.Guid]::Parse($objectId)
+foreach ($byte in $guid.ToByteArray()) {
+$byteGuid += [System.String]::Format("{0:X2}", $byte)
+}
+return "0x" + $byteGuid
+}
+$SID=ConvertTo-Sid($objId)
+Write-Verbose "Create SQL connectionstring"
+$conn = New-Object System.Data.SqlClient.SQLConnection
+$conn.ConnectionString = "Server=$db_server;Initial Catalog=$db_name;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30"
+$conn.AccessToken = $sqlToken
+Write-Verbose "Connect to database and execute SQL script"
+$conn.Open()
+$query = "IF NOT EXISTS (SELECT [name]
+FROM [sys].[database_principals]
+WHERE [type] = N'X' AND [name] = N'$aadGroupName')
+BEGIN
+CREATE USER [$aadGroupName] WITH DEFAULT_SCHEMA=[dbo], SID = $SID, TYPE= X;
+ALTER ROLE [$dbRoleName] ADD MEMBER [$aadGroupName];
+END
+"
+Write-Host $query
+$command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $conn)
+$Result = $command.ExecuteNonQuery()
+$conn.Close()
