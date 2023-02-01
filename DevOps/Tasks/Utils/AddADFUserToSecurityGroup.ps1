1c9011e7daae3db377e9770293ecc9588a5f229d
Clint Grove <clintelyonson@gmail.com>
2023-02-01 16:03:46 +0000

    nw ps1

--- DevOps/Tasks/Utils/AddADFUserToSecurityGroup.ps1
+++ DevOps/Tasks/Utils/AddADFUserToSecurityGroup.ps1
@@ -1,11 +1,31 @@
 param (
+    #[Parameter(Mandatory=$true)]
     [String] $AD_SecurityGroup ,
-    [String] $FactoryNameUpToEnv 
+    #[Parameter(Mandatory=$true)] 
+    [String] $FactoryNameUpToEnv
 )
 
 $idOfSecGroup = (Get-AzADGroup -DisplayName $AD_SecurityGroup).id
+write-host $idOfSecGroup
 $idOfFactory = (Get-AzADServicePrincipal -DisplayNameBeginsWith $FactoryNameUpToEnv).id
+write-host $idOfFactory
 
-if ($idOfFactory -eq $null) 
-{Add-AzADGroupMember -TargetGroupObjectId $idOfSecGroup -MemberObjectId $idOfFactory}
-else {Write-host already a member}
+$ErrorOccured = $false
+
+try 
+{ 
+   $ErrorActionPreference = 'Stop'
+   Add-AzADGroupMember -TargetGroupObjectId $idOfSecGroup -MemberObjectId $idOfFactory
+}
+catch
+{
+   "This user $FactoryNameUpToEnv is already a member"
+   $ErrorOccured=$true
+}
+
+if(!$ErrorOccured) {
+    Add-AzADGroupMember -TargetGroupObjectId $idOfSecGroup -MemberObjectId $idOfFactory -ErrorAction SilentlyContinue
+    Write-Host "memeber $FactoryNameUpToEnv is now added"
+}
+
+
